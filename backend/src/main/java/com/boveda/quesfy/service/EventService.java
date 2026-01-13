a884e9c3ab231f67466121a53d62693de3814319
socrater96 <nicoreisogo@gmail.com>
2026-01-13 14:20:39 +0100

    feature(events): added list events endpoint with controller unit tests

--- backend/src/main/java/com/boveda/quesfy/service/EventService.java
+++ backend/src/main/java/com/boveda/quesfy/service/EventService.java
@@ -3,8 +3,12 @@ package com.boveda.quesfy.service;
 import com.boveda.quesfy.domain.CreateEventRequest;
 import com.boveda.quesfy.domain.entity.Event;
 
+import java.util.List;
+
 public interface EventService {
 
     Event createEvent(CreateEventRequest request);
+
+    List<Event> listEvents();
 
 }
