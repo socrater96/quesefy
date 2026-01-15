5060204b4288d96d605ffb5f06ed8b2ca93c9610
socrater96 <nicoreisogo@gmail.com>
2026-01-15 20:09:16 +0100

    feat(venue): add Venue entity with embedded Location

---
+++ backend/src/main/java/com/boveda/quesefy/domain/entity/Venue.java
@@ -0,0 +1,25 @@
+package com.boveda.quesefy.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "venues")
+public class Venue {
+
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VenueType venueType;
+
+    @Embedded
+    private Location location;
+}
