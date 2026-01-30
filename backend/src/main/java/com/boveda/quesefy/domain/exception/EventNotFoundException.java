ee9bb805265d7bfb7b2c8166120cedb9b1da088f
socrater96 <nicoreisogo@gmail.com>
2026-01-30 21:26:40 +0100

    feature: added create endpoint to VenueController

--- backend/src/main/java/com/boveda/quesefy/domain/exception/EventNotFoundException.java
+++ backend/src/main/java/com/boveda/quesefy/domain/exception/EventNotFoundException.java
@@ -1,18 +1,16 @@
 package com.boveda.quesefy.domain.exception;
 
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
 public class EventNotFoundException extends RuntimeException{
     private final UUID id;
-
-    public UUID getId() {
-        return id;
-    }
 
     public EventNotFoundException(UUID id) {
         super("Event with id " + id + " not found");
         this.id = id;
-
 
     }
 }
