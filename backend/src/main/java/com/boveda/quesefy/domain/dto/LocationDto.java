a097be8a8832cab54e1681d47c8432fee31f40e5
socrater96 <nicoreisogo@gmail.com>
2026-02-04 17:53:19 +0100

    doc: add swagger to venue CRUD endpoints and dtos

--- backend/src/main/java/com/boveda/quesefy/domain/dto/LocationDto.java
+++ backend/src/main/java/com/boveda/quesefy/domain/dto/LocationDto.java
@@ -1,15 +1,29 @@
 package com.boveda.quesefy.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record LocationDto(
+        @Schema(example = "Kensington Gore")
         @NotNull String address,
+
+        @Schema(example = "London")
         @NotNull String city,
+
+        @Schema(example = "Greater London")
         @NotNull String province,
+
+        @Schema(example = "SW7 2AP")
         @NotNull String zipcode,
+
+        @Schema(example = "UK")
         @NotNull String country,
 
+
+        @Schema(example = "51.5010")
         @NotNull Double latitude,
+
+        @Schema(example = "-0.1773")
         @NotNull Double longitude
 ) {
 }
