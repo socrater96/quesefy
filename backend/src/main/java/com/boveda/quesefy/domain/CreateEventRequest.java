f4479cc598c9036243605687fe32f19f3a7bd080
socrater96 <nicoreisogo@gmail.com>
2026-02-05 19:39:48 +0100

    refactor/changed-venueDto-venueId

--- backend/src/main/java/com/boveda/quesefy/domain/CreateEventRequest.java
+++ backend/src/main/java/com/boveda/quesefy/domain/CreateEventRequest.java
@@ -1,14 +1,17 @@
 package com.boveda.quesefy.domain;
 
 import com.boveda.quesefy.domain.entity.EventType;
+import com.boveda.quesefy.domain.entity.Venue;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record CreateEventRequest(
         String title,
         String description,
         LocalDateTime date,
-        EventType type
+        EventType type,
+        UUID venueId
 ) {
 
 }
