67fe23bca0fc655861de75b7eafadabdf6a2b7fe
socrater96 <nicoreisogo@gmail.com>
2026-02-13 10:53:49 +0100

    refactor: migrate manual mappers to MapStruct

--- backend/src/main/java/com/boveda/quesefy/mapper/EventMapper.java
+++ backend/src/main/java/com/boveda/quesefy/mapper/EventMapper.java
@@ -6,13 +6,25 @@ import com.boveda.quesefy.domain.dto.CreateEventRequestDto;
 import com.boveda.quesefy.domain.dto.EventDto;
 import com.boveda.quesefy.domain.dto.UpdateEventRequestDto;
 import com.boveda.quesefy.domain.entity.Event;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface EventMapper {
 
     CreateEventRequest fromDto(CreateEventRequestDto requestDto);
 
+
     UpdateEventRequest fromDto(UpdateEventRequestDto requestDto);
 
+    @Mapping(
+            target = "venueId",
+            source = "venue.id"
+    )
     EventDto toDto(Event event);
 
 }
--- backend/src/main/java/com/boveda/quesefy/mapper/VenueMapper.java
+++ backend/src/main/java/com/boveda/quesefy/mapper/VenueMapper.java
@@ -4,16 +4,28 @@ package com.boveda.quesefy.mapper;
 import com.boveda.quesefy.domain.CreateVenueRequest;
 import com.boveda.quesefy.domain.UpdateVenueRequest;
 import com.boveda.quesefy.domain.dto.CreateVenueRequestDto;
+import com.boveda.quesefy.domain.dto.LocationDto;
 import com.boveda.quesefy.domain.dto.UpdateVenueRequestDto;
 import com.boveda.quesefy.domain.dto.VenueDto;
+import com.boveda.quesefy.domain.entity.Location;
 import com.boveda.quesefy.domain.entity.Venue;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface VenueMapper {
 
-    CreateVenueRequest fromDto(CreateVenueRequestDto createVenueRequestDto);
+    CreateVenueRequest fromDto(CreateVenueRequestDto dto);
+
+    UpdateVenueRequest fromDto(UpdateVenueRequestDto dto);
 
     VenueDto toDto(Venue venue);
 
-    UpdateVenueRequest fromDto(UpdateVenueRequestDto updateVenueRequestDto);
+    Location fromDto(LocationDto dto);
+
+    LocationDto toDto(Location location);
 
 }
